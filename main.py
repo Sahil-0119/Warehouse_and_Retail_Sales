9bb862ba2140651a4781c495e586e7b64f489264
Sahil Beniwal <151610799+Sahil-0119@users.noreply.github.com>
2025-03-26 19:54:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ item_type= data['ITEM TYPE'].value_counts() # value_counts() is used to count un
 # Creating pie chart 
 plt.figure(figsize=(10, 8))  # Create a figure with the specified size
 c= ['sandybrown', 'royalblue', 'navy', 'crimson', 'thistle', 'purple', 'pink']
-plt.pie(item_type, labels=item_type.index, colors=c,autopct='%0.2f%%')
+plt.pie(item_type, labels=item_type.index, colors=c,startangle=140,autopct='%0.2f%%')
 plt.title('Distribution of Item Types with Percentage')
 plt.show()
 
